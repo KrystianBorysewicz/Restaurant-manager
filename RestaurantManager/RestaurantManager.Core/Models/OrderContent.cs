47d0bba1ca4f254c1cb4dcbb4683646bd733ddee
Krystian Borysewicz <krystian.borysewicz@gmail.com>
2019-12-17 05:29:03 +0100

    Create OrderContent.cs

---
+++ RestaurantManager/RestaurantManager.Core/Models/OrderContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Core.Models
+{
+    public class OrderContent
+    {
+        public int OrderContentId { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string AdditionalInformation { get; set; }
+    }
+}
